e87de1265c87cefd0ccb124744efec95c0cc7d67
Tommyttoe <tommy.nguyen195@gmail.com>
2025-06-26 14:44:45 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import vercel from '@vercel/remix';
+import vercelPreset from '@vercel/remix';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const { vercelPreset } = vercel;
-
-/** @type {import('@vercel/remix').VercelViteConfig} */
 export default {
   ...vercelPreset(),
   plugins: [tsconfigPaths()],
